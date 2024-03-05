90761eae9570b3d34f7edcd3df701b7f62e62f7f
Alejandro Esteban Muñoz Osorio <almunozo@unal.edu.co>
2024-03-04 20:16:58 -0500

    Solucion

--- country_scientific_prodcution.py
+++ country_scientific_prodcution.py
@@ -1,1 +1,81 @@
 """Taller Presencial Evaluable"""
+
+import pandas as pd
+import folium
+
+
+def load_affiliations():
+    """Load affiliations from scopus-papers.csvi"""
+    dataframe = pd.read_csv("https://raw.githubusercontent.com/jdvelasq/datalabs/master/datasets/scopus-papers.csv", sep=",", index_col=None)[['Affiliations']]
+    return dataframe
+
+def remove_na_rows(affiliations):
+    """Elimina las filas con valores nulos en la columna 'Affiliations'"""
+    affiliations = affiliations.copy()
+    affiliations = affiliations.dropna(subset=["Affiliations"])
+    return affiliations
+
+def add_countries_column(affiliations):
+    """Transforma la columna 'Affiliations' a una lista de paises."""
+
+    affiliations = affiliations.copy()
+    affiliations["countries"] = affiliations["Affiliations"].copy()
+    affiliations["countries"] = affiliations["countries"].str.split(";")
+    affiliations["countries"] = affiliations["countries"].map(
+        lambda x: [y.split(",") for y in x]
+    )
+    affiliations["countries"] = affiliations["countries"].map(
+        lambda x: [y[-1].strip() for y in x]
+    )
+    affiliations["countries"] = affiliations["countries"].map(set)
+    affiliations["countries"] = affiliations["countries"].str.join(", ")
+
+    return affiliations
+  
+def clean_countries(affiliations):
+
+    affiliations = affiliations.copy()
+    affiliations["countries"] = affiliations["countries"].str.replace(
+        "United States", "United States of America"
+    )
+    return affiliations
+
+def count_country_frequency(affiliations):
+    """Cuenta la frecuencia de cada país en la columna 'countries'"""
+
+    countries = affiliations["countries"].copy()
+    countries = countries.str.split(", ")
+    countries = countries.explode()
+    countries = countries.value_counts()
+    return countries
+
+def plot_world_map(countries):
+    """Grafica un mapa mundial con la frecuencia de cada país."""
+
+    countries = countries.copy()
+    countries = countries.to_frame()
+    countries = countries.reset_index()
+
+    m = folium.Map(location=[0, 0], zoom_start=2)
+
+    folium.Choropleth(
+        geo_data="https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/world-countries.json",
+        data=countries,
+        columns=["countries", "count"],
+        key_on="feature.properties.name",
+        fill_color="Greens",
+    ).add_to(m)
+    m.save("map.html")
+  
+def main():
+    """Función principal"""
+    affiliations = load_affiliations()
+    affiliations = remove_na_rows(affiliations)
+    affiliations = add_countries_column(affiliations)
+    affiliations = clean_countries(affiliations)
+    countries = count_country_frequency(affiliations)
+    countries.to_csv("countries.csv")
+    plot_world_map(countries)
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -16,13 +16,13 @@ if not os.path.exists("countries.csv"):
 #
 # Lee el contenido del archivo output.txt
 dataframe = pd.read_csv("countries.csv")
-dataframe = dataframe.set_index("country")
+dataframe = dataframe.set_index("countries")
 
-assert dataframe["United States of America"] == 579
-assert dataframe["China"] == 273
-assert dataframe["India"] == 174
-assert dataframe["United Kingdom"] == 173
-assert dataframe["Italy"] == 112
+assert dataframe.loc["United States of America"]["count"] == 579
+assert dataframe.loc["China"]["count"] == 273
+assert dataframe.loc["India"]["count"] == 174
+assert dataframe.loc["United Kingdom"]["count"] == 173
+assert dataframe.loc["Italy"]["count"] == 112
 
 if not os.path.exists("map.html"):
     raise FileNotFoundError("File 'map.html' not found")
